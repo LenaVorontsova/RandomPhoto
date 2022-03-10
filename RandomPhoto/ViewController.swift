018c1d255a07b4fb9542d1f8d10ed664e11be0dc
LenaVorontsova <lena.voroncova26@yandex.ru>
2022-03-10 16:11:16 +0400

    add: add random photo to the imageView

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -18,12 +18,22 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .systemPink
         view.addSubview(imageView)
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         imageView.center = view.center
+        
+        getRandomPhoto()
     }
 
-
+    func getRandomPhoto() {
+        let urlString = "https://source.unsplash.com/random/600x600"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
+    }
 }
 
